e0f7aa59608de65ac0a5904de21d77965e9cf37a
mohamedtamer0 <mt4452242@gmail.com>
2022-03-05 21:30:51 +0200

    update

--- MySecondApp/app/src/main/java/com/example/mysecondapp/MainActivity.kt
+++ MySecondApp/app/src/main/java/com/example/mysecondapp/MainActivity.kt
@@ -4,39 +4,78 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textTitle: TextView
     lateinit var buttonClick: Button
-    var counter = 0
+    lateinit var switchMat: SwitchMaterial
+    lateinit var textUserName: EditText
+    lateinit var textTitle: TextView
+    lateinit var img: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textTitle = findViewById(R.id.text_title)
-        textTitle.text = "This is First App"
-
-        buttonClick = findViewById(R.id.btn_click)
-        buttonClick.text = "Don't Click Here"
-
-        Log.d("MyTest","Counter: $counter")
-
-//        buttonClick.setOnClickListener {
-//            counter++
-//            textTitle.text = "Counter $counter"
-//        }
-
+        initView()
+        addCallbacks()
 
     }
 
-    fun onClickedBtn(view: View) {
-        counter++
-        Log.d("MyTest","Clicked & Counter: $counter")
-        textTitle.text = "Counter $counter"
+    private fun addCallbacks() {
+//        switchMat.setOnCheckedChangeListener { compoundButton, b ->
+//            buttonClick.isEnabled = b
+//        }
+//        textUserName.addTextChangedListener { text ->
+//            Log.v(LOG_TAG, text.toString())
+//        }
+
+        buttonClick.setOnClickListener {
+//            showMessage()
+//            updateTitle()
+//            updateColor()
+            updateLogo()
+        }
+
+    }
+
+
+    private fun updateLogo() {
+        val mIcon = ContextCompat.getDrawable(this, R.drawable.ic_android_black_24dp)
+        img.setImageDrawable(mIcon)
+    }
+
+
+    private fun updateColor() {
+        val purple = ContextCompat.getColor(this, R.color.purple_200)
+        textTitle.setBackgroundColor(purple)
+    }
+
+
+    private fun updateTitle() {
+        textTitle.text = getString(R.string.my_name)
+    }
+
+
+//    private fun showMessage() {
+//        val msg = textUserName.text.toString()
+//        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+//    }
+
+
+    private fun initView() {
+        buttonClick = findViewById(R.id.btn_click)
+        switchMat = findViewById(R.id.switch_username)
+        textUserName = findViewById(R.id.text_username)
+        textTitle = findViewById(R.id.txtTitle)
+        img = findViewById(R.id.logo)
+    }
+
+    companion object {
+        const val LOG_TAG = "MyActivity"
     }
 
 }
