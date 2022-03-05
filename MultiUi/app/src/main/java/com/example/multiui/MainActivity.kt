8f1198938ac37c78ae4eb4437958a32e289b4047
mohamedtamer0 <mt4452242@gmail.com>
2022-03-05 23:43:12 +0200

    update

--- MultiUi/app/src/main/java/com/example/multiui/MainActivity.kt
+++ MultiUi/app/src/main/java/com/example/multiui/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.multiui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.multiui.MainActivity.Constant.extraName
+import com.example.multiui.databinding.ActivityMain2Binding
+import com.example.multiui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addCallbacks()
     }
+
+    private fun addCallbacks() {
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            val name = binding.textName.text.toString()
+            intent.putExtra(extraName, name)
+            startActivity(intent)
+        }
+    }
+
+    object Constant {
+        val extraName = "extraName"
+    }
+
 }
--- MultiUi/app/src/main/java/com/example/multiui/MainActivity2.kt
+++ MultiUi/app/src/main/java/com/example/multiui/MainActivity2.kt
@@ -2,10 +2,18 @@ package com.example.multiui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.multiui.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra(MainActivity.Constant.extraName)
+        binding.textData.text = name
+
     }
 }
