3f1269ab5bf00ea38521afb05c13e2987290c534
mohamedtamer0 <mt4452242@gmail.com>
2022-03-05 23:07:06 +0200

    update

---
+++ MultiUi/app/src/main/java/com/example/multiui/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.multiui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
