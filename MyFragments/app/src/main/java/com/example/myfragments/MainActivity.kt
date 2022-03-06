4bd9542b8231358e61cc65144a53bc813272076f
mohamedtamer0 <mt4452242@gmail.com>
2022-03-06 21:52:23 +0200

    update

--- MyFragments/app/src/main/java/com/example/myfragments/MainActivity.kt
+++ MyFragments/app/src/main/java/com/example/myfragments/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.myfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    val myFirstFragment = OneFragment()
+    val mySecondFragment = TwoFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSubView()
+        initButton()
     }
+
+    private fun initButton() {
+        findViewById<Button>(R.id.button_change).setOnClickListener {
+            showSecondFragment()
+        }
+
+        findViewById<Button>(R.id.button_remove).setOnClickListener {
+            removeFragment(mySecondFragment)
+        }
+
+    }
+
+
+    private fun removeFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.remove(fragment)
+        transaction.commit()
+    }
+
+    private fun showSecondFragment() {
+        replaceFragment(mySecondFragment)
+    }
+
+    private fun initSubView() {
+        addFragment(myFirstFragment)
+    }
+
+
+    private fun addFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+
 }
--- MyFragments/app/src/main/java/com/example/myfragments/TwoFragment.kt
+++ MyFragments/app/src/main/java/com/example/myfragments/TwoFragment.kt
@@ -1,6 +1,8 @@
 package com.example.myfragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,63 @@ class TwoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(OneFragment.LOG_TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(LOG_TAG, "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(LOG_TAG, "onCreate")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //deprecated don't use it
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(LOG_TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(LOG_TAG, "onDetach")
+    }
+
+
+    companion object {
+        const val LOG_TAG = "FFF_Two"
     }
 }
