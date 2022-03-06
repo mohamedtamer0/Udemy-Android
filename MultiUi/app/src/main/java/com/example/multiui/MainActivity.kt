0d86b754f3eb81433abb0b7ae819805bc48ca5ee
mohamedtamer0 <mt4452242@gmail.com>
2022-03-06 18:08:18 +0200

    update

--- MultiUi/app/src/main/java/com/example/multiui/MainActivity.kt
+++ MultiUi/app/src/main/java/com/example/multiui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.multiui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.multiui.MainActivity.Constant.extraName
 import com.example.multiui.databinding.ActivityMain2Binding
 import com.example.multiui.databinding.ActivityMainBinding
@@ -14,9 +16,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Log.d(LOG_TAG, "onCreate")
         addCallbacks()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
+    }
+
+    companion object {
+        const val LOG_TAG = "FIRST_ACTIVITY"
+    }
+
 
     private fun addCallbacks() {
         binding.button.setOnClickListener {
--- MultiUi/app/src/main/java/com/example/multiui/MainActivity2.kt
+++ MultiUi/app/src/main/java/com/example/multiui/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.example.multiui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.multiui.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -11,9 +13,52 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d(LOG_TAG, "onCreate")
 
         val name = intent.getStringExtra(MainActivity.Constant.extraName)
         binding.textData.text = name
+        addCallbacks()
+    }
 
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
+    }
+
+    companion object {
+        const val LOG_TAG = "SECOND_ACTIVITY"
+    }
+
+
+    private fun addCallbacks() {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
     }
 }
