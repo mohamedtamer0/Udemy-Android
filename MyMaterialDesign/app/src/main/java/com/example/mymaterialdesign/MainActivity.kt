48e9dab948c3b5197eccc27ace049c385f35f109
mohamedtamer0 <mt4452242@gmail.com>
2022-03-08 18:26:17 +0200

    update

--- MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
+++ MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mymaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mymaterialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+            Toast.makeText(this, "You Clicked Me", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
