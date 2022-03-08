efbc99a7d0d096465e496bdaf8f51dae100d5c46
mohamedtamer0 <mt4452242@gmail.com>
2022-03-08 23:40:14 +0200

    update

--- MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
+++ MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mymaterialdesign
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.mymaterialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,30 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "You Clicked Me", Toast.LENGTH_LONG).show()
         }
 
+        binding.btnDialog.setOnClickListener {
+            showMeDialog()
+        }
 
     }
+
+
+    private fun showMeDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setTitle("title")
+            setMessage("message")
+            setPositiveButton("yes", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(this@MainActivity, "Positive button Clicked", Toast.LENGTH_LONG)
+                    .show()
+            })
+            setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(this@MainActivity, "Negative button Clicked", Toast.LENGTH_LONG)
+                    .show()
+            })
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+
 }
