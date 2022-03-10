eeedaff556415377ae90263704e67eec9d94f055
mohamedtamer0 <mt4452242@gmail.com>
2022-03-10 19:49:40 +0200

    update

---
+++ MyCollapsingToolbar/app/src/main/java/com/example/mycollapsingtoolbar/ScrollingActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mycollapsingtoolbar
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mycollapsingtoolbar.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
