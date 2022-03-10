5d8f80fd588b822eb979a4ce8fd4843083cf4837
mohamedtamer0 <mt4452242@gmail.com>
2022-03-10 22:35:17 +0200

    update

--- MyNavigationDrawer/app/src/main/java/com/example/mynavigationdrawer/MainActivity.kt
+++ MyNavigationDrawer/app/src/main/java/com/example/mynavigationdrawer/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.mynavigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(this, "Clicked Home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.nav_share -> {
+                    Toast.makeText(this, "Clicked Share", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.nav_message -> {
+                    Toast.makeText(this, "Clicked Message", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.nav_person -> {
+                    Toast.makeText(this, "Clicked Person", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
