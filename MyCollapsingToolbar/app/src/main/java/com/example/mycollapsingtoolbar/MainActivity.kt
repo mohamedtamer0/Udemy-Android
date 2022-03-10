63c37a2ddfb0cdac9eaedf7c856bacbfd6e266f6
mohamedtamer0 <mt4452242@gmail.com>
2022-03-10 20:01:15 +0200

    update

--- MyCollapsingToolbar/app/src/main/java/com/example/mycollapsingtoolbar/MainActivity.kt
+++ MyCollapsingToolbar/app/src/main/java/com/example/mycollapsingtoolbar/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.mycollapsingtoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.mycollapsingtoolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+        val items = listOf("A", "B", "C", "D")
+        val myAdapter = ArrayAdapter(this, R.layout.spinner_item, items)
+
+        binding.spinner.apply {
+            adapter = myAdapter
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    Toast.makeText(this@MainActivity, items[p2], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                }
+
+            }
+        }
+
     }
 }
