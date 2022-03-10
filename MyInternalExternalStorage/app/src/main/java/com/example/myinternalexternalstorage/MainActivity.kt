19cf13d2162dfb53f595f0fa9625228f39d8ca5d
mohamedtamer0 <mt4452242@gmail.com>
2022-03-11 01:01:55 +0200

    update

--- MyInternalExternalStorage/app/src/main/java/com/example/myinternalexternalstorage/MainActivity.kt
+++ MyInternalExternalStorage/app/src/main/java/com/example/myinternalexternalstorage/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myinternalexternalstorage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import com.example.myinternalexternalstorage.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addListeners()
     }
+
+
+    private fun addListeners() {
+        binding.save.setOnClickListener {
+            val text = binding.edittext.text.toString()
+            saveTextToFile(text)
+            Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun saveTextToFile(text: String) {
+        val path = Environment.getExternalStorageDirectory().path
+        val fileName = "sample.txt"
+        val file = File("$path/$fileName")
+        file.writeText(text)
+    }
+
+
 }
