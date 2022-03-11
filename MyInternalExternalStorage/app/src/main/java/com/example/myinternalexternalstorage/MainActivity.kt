fb42e0285e7fa76082fd2a2525aad3c5c239504f
mohamedtamer0 <mt4452242@gmail.com>
2022-03-11 18:52:06 +0200

    update

--- MyInternalExternalStorage/app/src/main/java/com/example/myinternalexternalstorage/MainActivity.kt
+++ MyInternalExternalStorage/app/src/main/java/com/example/myinternalexternalstorage/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.myinternalexternalstorage
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.myinternalexternalstorage.databinding.ActivityMainBinding
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+
+            } else {
+
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +36,18 @@ class MainActivity : AppCompatActivity() {
     private fun addListeners() {
         binding.save.setOnClickListener {
             val text = binding.edittext.text.toString()
-            saveTextToFile(text)
-            Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                saveTextToFile(text)
+                Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+
+
         }
     }
 
