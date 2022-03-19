7c82586f46705f1ba601872b5891040174548809
mohamedtamer0 <mt4452242@gmail.com>
2022-03-19 19:10:12 +0200

    update

--- MyServices/app/src/main/java/com/example/myservices/MainActivity.kt
+++ MyServices/app/src/main/java/com/example/myservices/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myservices
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val intent = Intent(this, MyService::class.java)
-        startService(intent)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+
+
+
+
     }
 }
--- MyServices/app/src/main/java/com/example/myservices/MyService.kt
+++ MyServices/app/src/main/java/com/example/myservices/MyService.kt
@@ -1,20 +1,46 @@
 package com.example.myservices
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.widget.Toast
 
-class MyService:Service() {
+class MyService : Service() {
+
+    val SERVICE_ID = 1
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
-//    override fun onCreate() {
-//        super.onCreate()
-//    }
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channelId = "my_channel_1"
+            val channel =
+                NotificationChannel(channelId, "Default", NotificationManager.IMPORTANCE_DEFAULT)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+            val notification =
+                Notification.Builder(this, channelId).apply {
+                    setContentTitle("notification title")
+                    setContentText("this is content")
+                    setSmallIcon(R.drawable.ic_launcher_foreground)
+                }.build()
+
+            startForeground(SERVICE_ID, notification)
+        }
+
+
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         showMessage()
@@ -26,7 +52,7 @@ class MyService:Service() {
             {
                 Toast.makeText(applicationContext, "Hello World", Toast.LENGTH_SHORT).show()
             },
-            5000
+            10000
         )
     }
 
