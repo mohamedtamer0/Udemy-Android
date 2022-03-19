03bed34296926f441d909a5736b96eceb406e9a3
mohamedtamer0 <mt4452242@gmail.com>
2022-03-19 17:43:49 +0200

    update

--- MyReadSMS/app/src/main/java/com/example/myreadsms/MainActivity.kt
+++ MyReadSMS/app/src/main/java/com/example/myreadsms/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.myreadsms
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.myreadsms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getSms()
+    }
+
+    private fun getSms() {
+        val uri = Uri.parse("content://sms//inbox")
+        val projection = arrayOf(SMS_SENDER, SMS_BODY)
+        val cursor: Cursor = contentResolver.query(uri, projection, null, null, null)!!
+        while (cursor.moveToNext()) {
+            for (i in 0 until cursor.columnCount) {
+                Log.i(LOG_TAG, "$i - ${cursor.getColumnName(i)} - ${cursor.getString(i)} ")
+            }
+        }
+    }
+
+    companion object {
+        private const val LOG_TAG = "MAIN_ACTIVTY"
+        private const val SMS_BODY = "body"
+        private const val SMS_SENDER = "address"
     }
 }
