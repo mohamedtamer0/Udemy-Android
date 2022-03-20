3dc829ad278af68ff6b5d60189e1419d49086af1
mohamedtamer0 <mt4452242@gmail.com>
2022-03-20 22:42:44 +0200

    update

--- MyRXJava/app/src/main/java/com/example/myrxjava/MainActivity.kt
+++ MyRXJava/app/src/main/java/com/example/myrxjava/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.myrxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myrxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +20,48 @@ class MainActivity : AppCompatActivity() {
         foo()
     }
 
-
     private fun foo() {
+        val happy = Observable.just(5,10,50,100,660,990)
 
+        val observer = object : Observer<Int> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG, "onSubscribe: ")
+            }
+
+            override fun onNext(t: Int) {
+                Log.d(TAG, "onNext: $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError: ")
+            }
+
+            override fun onComplete() {
+                Log.d(TAG, "onComplete: ")
+            }
+
+        }
+
+        happy.subscribe(observer)
+    }
+
+
+//    private fun foo() {
+//        var foo = 5
+//        Log.d(TAG, foo.toString())
+//        foo = 10
+//        Log.d(TAG, foo.toString())
+//        foo = 66
+//        Log.d(TAG, foo.toString())
+//        foo = 90
+//        Log.d(TAG, foo.toString())
+//        foo = 550
+//        Log.d(TAG, foo.toString())
+//        foo = 800
+//        Log.d(TAG, foo.toString())
+//    }
+
+    companion object {
+        const val TAG = "Tamer"
     }
 }
