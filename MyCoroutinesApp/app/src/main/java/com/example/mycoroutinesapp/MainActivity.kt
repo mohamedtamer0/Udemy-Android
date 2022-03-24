b40a97eb39814444a379c679d8deafca5da17c0b
mohamedtamer0 <mt4452242@gmail.com>
2022-03-24 19:57:15 +0200

    update

--- MyCoroutinesApp/app/src/main/java/com/example/mycoroutinesapp/MainActivity.kt
+++ MyCoroutinesApp/app/src/main/java/com/example/mycoroutinesapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.mycoroutinesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.mycoroutinesapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            fakeApiRequest()
+        }
+        Log.d(TAG, "after Coroutine")
+
+//        runBlocking {
+//            fakeApiRequest()
+//        }
+    }
+
+    suspend fun fakeApiRequest(){
+        delay(2500)
+        Log.d(TAG, "fakeApiRequest")
+    }
+
+    companion object {
+        private const val TAG = "Tamer"
     }
 }
