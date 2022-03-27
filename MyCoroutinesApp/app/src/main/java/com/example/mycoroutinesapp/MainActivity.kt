cb2fd79a3fa73ffe2b8d73030d18740284297152
mohamedtamer0 <mt4452242@gmail.com>
2022-03-27 23:03:59 +0200

    update

--- MyCoroutinesApp/app/src/main/java/com/example/mycoroutinesapp/MainActivity.kt
+++ MyCoroutinesApp/app/src/main/java/com/example/mycoroutinesapp/MainActivity.kt
@@ -19,13 +19,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val jobParent = lifecycleScope.launch {
-            val deferred1 = async { repeatLogs1() }
-            val deferred2 = async { repeatLogs2() }
-
-            Log.d(TAG, deferred1.await())
-            Log.d(TAG, deferred2.await())
-        }
+//        val jobParent = lifecycleScope.launch {
+//            val deferred1 = async { repeatLogs1() }
+//            val deferred2 = async { repeatLogs2() }
+//
+//            Log.d(TAG, deferred1.await())
+//            Log.d(TAG, deferred2.await())
+//        }
 
 
         //Log.d(TAG, "after Coroutine")
@@ -33,6 +33,41 @@ class MainActivity : AppCompatActivity() {
 //        runBlocking {
 //            fakeApiRequest()
 //        }
+
+        playWithCoroutine()
+        binding.btnCancel.setOnClickListener {
+            job4.cancel()
+        }
+    }
+
+    lateinit var job1: Job
+    lateinit var job2: Job
+    lateinit var job3: Job
+    lateinit var job4: Job
+    lateinit var job5: Job
+
+
+    private fun playWithCoroutine() {
+        job1 = lifecycleScope.launch {
+            delay(2000)
+            job2 = launch {
+                delay(2000)
+                Log.d(TAG, "I am job2")
+                job4 = launch {
+                    delay(2000)
+                    Log.d(TAG, "I am job4")
+                }
+                job5 = launch {
+                    delay(2000)
+                    Log.d(TAG, "I am job5")
+                }
+            }
+            job3 = launch {
+                delay(2000)
+                Log.d(TAG, "I am job3")
+            }
+            Log.d(TAG, "I am job1")
+        }
     }
 
 
